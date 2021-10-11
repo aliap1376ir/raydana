5f308646abe18e637c09f6600a59bf9385a7271d
Ali Azizi Pour <aliap1376ir@gmail.com>
2021-10-11 20:52:28 +0330

    Spring Part Raydana Docker Add

--- docker/JarSpringBoot/src/main/java/com/raydana/Controller.java
+++ docker/JarSpringBoot/src/main/java/com/raydana/Controller.java
@@ -1,2 +1,15 @@
-package com.raydana;public class Controller {
+package com.raydana;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping(value = "/")
+    private String helloWorld() {
+        String code = SlugGenerator.generate(SlugGenerator.Options.GENERATE_FROM_NUMBERS_AND_LETTERS, 10);
+        System.out.println(code);
+        return code;
+    }
 }
