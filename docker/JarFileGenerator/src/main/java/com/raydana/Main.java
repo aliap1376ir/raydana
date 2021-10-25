0aaac44865c1880151a8592854dddd7cb0320b63
Ali Azizi Pour <aliap1376ir@gmail.com>
2021-10-25 05:48:34 +0330

    final jar created

--- docker/JarFileGenerator/src/main/java/com/raydana/Main.java
+++ docker/JarFileGenerator/src/main/java/com/raydana/Main.java
@@ -1,14 +1,32 @@
 package com.raydana;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        while (true) {
-            System.out.println(SlugGenerator.generate(SlugGenerator.Options.GENERATE_FROM_NUMBERS_AND_LETTERS, 10));
-            try {
-                Thread.sleep(10000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        try {
+            String content = SlugGenerator.generate(SlugGenerator.Options.GENERATE_FROM_NUMBERS_AND_LETTERS, 10);
+            File file = new File("Ray", new Date().getTime() + ".txt");
+            file.getParentFile().mkdirs();
+            FileOutputStream fout = new FileOutputStream(file);
+            fout.write(content.getBytes(StandardCharsets.UTF_8));
+            fout.flush();
+            fout.close();
+            System.out.println(file.getPath() + " is generated with content " + content);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
